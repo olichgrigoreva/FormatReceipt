fba70bb7c98d27ca51ef3a3e0bd2fb9e1867fbfd
Olga <aboutrocky94@gmail.com>
2020-08-09 16:14:04 +0300

    вывод чека на экран

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.Formatter;
 
 public class MainApp {
     public static void main(String[] args) {
@@ -9,29 +8,18 @@ public class MainApp {
             float quantity, price, cost, total = 0;
             System.out.format("%-15s %10s   %-10s %-10s\n", "Наименование", "Кол-во", "Цена", "Стоимость");
             line();
-            /*while ((prodName = lnr.readLine()) != null) {
-                quantity = Float.parseFloat(lnr.readLine());
-                price = Float.parseFloat(lnr.readLine());
-                cost = quantity * price;
-                total += cost;
-                System.out.format("%-15s %10s x %-10.2f =%-10.2f\n", prodName, quantity, price, cost);
-            }*/
-            try (FileWriter fw = new FileWriter(new File("out.txt"))){
-                do {
-                    if ((prodName = lnr.readLine()) == null) {
-                        line();
-                        System.out.format("ИТОГО: %34s%7.2f", "=", total);
-                    } else {
-                        quantity = Float.parseFloat(lnr.readLine());
-                        price = Float.parseFloat(lnr.readLine());
-                        cost = quantity * price;
-                        total += cost;
-                        System.out.format("%-15s %10s x %-10.2f =%-10.2f\n", prodName, quantity, price, cost);
-                    }
-                } while (prodName != null);
-            } catch (Exception ex) {
-
-            }
+            do {
+                if ((prodName = lnr.readLine()) == null) {
+                    line();
+                    System.out.format("ИТОГО: %34s%7.2f", "=", total);
+                } else {
+                    quantity = Float.parseFloat(lnr.readLine());
+                    price = Float.parseFloat(lnr.readLine());
+                    cost = quantity * price;
+                    total += cost;
+                    System.out.format("%-15s %10s x %-10.2f =%-10.2f\n", prodName, quantity, price, cost);
+                }
+            } while (prodName != null);
         } catch (IOException ioException) {
             ioException.printStackTrace();
         }
