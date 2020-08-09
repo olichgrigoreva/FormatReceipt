7b7e00ba3e7e1fbb3d8b5af9e886e41e9e5204a0
Olga <aboutrocky94@gmail.com>
2020-08-09 17:02:06 +0300

    вывод чека на экран + запись в файл

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,4 +1,6 @@
+import javax.xml.crypto.Data;
 import java.io.*;
+import java.util.Formatter;
 
 public class MainApp {
     public static void main(String[] args) {
@@ -6,31 +8,44 @@ public class MainApp {
              LineNumberReader lnr = new LineNumberReader(fr)) {
             String prodName;
             float quantity, price, cost, total = 0;
-            System.out.format("%-15s %10s   %-10s %-10s\n", "Наименование", "Кол-во", "Цена", "Стоимость");
-            line();
-            do {
-                if ((prodName = lnr.readLine()) == null) {
-                    line();
-                    System.out.format("ИТОГО: %34s%7.2f", "=", total);
-                } else {
-                    quantity = Float.parseFloat(lnr.readLine());
-                    price = Float.parseFloat(lnr.readLine());
-                    cost = quantity * price;
-                    total += cost;
-                    System.out.format("%-15s %10s x %-10.2f =%-10.2f\n", prodName, quantity, price, cost);
-                }
-            } while (prodName != null);
+            try (Formatter fileFormatter = new Formatter(new FileWriter(new File("out.txt"))))
+            {
+                format(fileFormatter);
+                System.out.format("%-15s %10s   %-10s %-10s\n", "Наименование", "Кол-во", "Цена", "Стоимость");
+                System.out.print("=======================================================\n");
+                do {
+                    if ((prodName = lnr.readLine()) == null) {
+                        System.out.print("=======================================================\n");
+                        format(fileFormatter, total);
+                        System.out.format("ИТОГО: %34s%7.2f", "=", total);
+                    } else {
+                        quantity = Float.parseFloat(lnr.readLine());
+                        price = Float.parseFloat(lnr.readLine());
+                        cost = quantity * price;
+                        total += cost;
+                        format(fileFormatter, prodName, quantity, price, cost);
+                        System.out.format("%-15s %10s x %-10.2f =%-10.2f\n", prodName, quantity, price, cost);
+                    }
+                } while (prodName != null);
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
         } catch (IOException ioException) {
             ioException.printStackTrace();
         }
     }
 
-    private static void line() {
-        for (int i = 0; i < 49; i++) {
-            System.out.print("=");
-            if (i == 48) {
-                System.out.println("");
-            }
-        }
+    private static void format(Formatter formatter) {
+        formatter.format("%-15s %10s   %-10s %-10s\n", "Наименование", "Кол-во", "Цена", "Стоимость");
+        formatter.format("=======================================================\n");
+    }
+
+    private static void format(Formatter formatter, float total) {
+        formatter.format("=======================================================\n");
+        formatter.format("ИТОГО: %34s%7.2f", "=", total);
+    }
+
+    private static void format(Formatter formatter, String prodName, float quantity, float price, float cost) {
+        formatter.format("%-15s %10s x %-10.2f =%-10.2f\n", prodName, quantity, price, cost);
     }
 }
